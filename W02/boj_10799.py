0813f3edf8106d3dc222e4e1340c7ac7369ffec9
qloeo <https://github.com/qloeo>
2023-07-11 00:14:29 +0900

    [UPDATE]2주차 수정

--- W02/boj_10799.py
+++ W02/boj_10799.py
@@ -1,18 +1,18 @@
 def solution():
     arrangement = input().strip()
-    stack = []
-    answer = 0
+    stack = []  # 쇠막대기를 임시로 저장하기 위한 스택
+    answer = 0  # 잘린 쇠막대기 조각의 개수를 저장한 변수
 
-    for i in range(len(arrangement)):
-        if arrangement[i] == '(':
-            stack.append('(')
-        else:
-            if arrangement[i-1] == '(':
-                stack.pop()
+    for i in range(len(arrangement)):  # 입력받은 문자열의 각 문자에 대해 반복
+        if arrangement[i] == '(':  # 현재 문자가 여는 괄호인 경우
+            stack.append('(')      # 여는 괄호를 만나면, 쇠막대기의 시작을 의미하므로 스택에 추가
+        else:  # 현재 문자가 닫는 괄호인 경우
+            if arrangement[i-1] == '(':  # 바로 앞의 문자가 여는 괄호인 경우 (레이저)
+                stack.pop()  # 레이저를 스택에서 제거
                 answer += len(stack)
-            else:
-                stack.pop()
-                answer += 1
+            else:  #  바로 앞의 문자가 여는 괄호가 아닌 경우 (쇠막대기의 끝)
+                stack.pop()  # 쇠막대기의 끝을 스택에서 제거
+                answer += 1  # 쇠막대기의 마지막 부분이므로, 조각의 개수를 1개 추가
 
     print(answer)
 
--- W02/programmers_lv2_178870.py
+++ W02/programmers_lv2_178870.py
@@ -1,21 +1,24 @@
-def solution(sequence, k):
-    start = 0
-    end = 0
-    current_sum = sequence[0]
-    result = [0, len(sequence)-1]
 
-    while end < len(sequence):
-        if current_sum == k:
-            if end - start < result[1] - result[0]:
-                result = [start, end]
-            current_sum -= sequence[start]
-            start += 1
-        elif current_sum < k:
-            end += 1
-            if end < len(sequence):
-                current_sum += sequence[end]
-        else: 
-            current_sum -= sequence[start]
-            start += 1
+# 슬라이딩 윈도우 알고리즘을 사용하여 문제를 해결. 
+def solution(sequence, k):
+    start = 0  # 시작index 저장
+    end = 0    # 끝index 저장
+    current_sum = sequence[0]  # 원소들의 합을 저장
+    result = [0, len(sequence)-1]  # 가장 짧은 수열 부분의 시작과 끝 인덱스
+
+    while end < len(sequence):  # end 인덱스가 수열 끝에 도달 할 때까지 실행!
+        if current_sum == k:  # current_sum이 우리가 찾는 합 'k'와 같은지 확인
+            if end - start < result[1] - result[0]:  # 현재 찾은 수열의 시작과 끝이 기존 수열의 길이보다 짧은지 확인 (end - start) 
+                result = [start, end]  # 짧으면 값을 갱신
+            current_sum -= sequence[start]  # strat 인덱스에 있는 값을 빼준다.
+            start += 1  # 윈도우의 start값이 오른쪽으로 이동 (윈도우 축소)
+        elif current_sum < k:  # current_sum이 우리가 찾는 합 'k'보다 작을때
+            end += 1  # 윈도우의 end값을 오른쪽으로 이동 (윈도우 확대)
+            if end < len(sequence):  # 수열의 총 길이보다 작으면
+                current_sum += sequence[end]  # 원소들의 합에 추가
+        else:  # current_sum이 우리가 찾는 합 'k'보다 큰 경우
+            current_sum -= sequence[start]  # start 인덱스에 있는 값을 빼준다.
+            start += 1  # start 값을 오른쪽으로 이동
             
     return result
+
