09b7ca3a621d8a72e59d0c3d2eefb0b6e3b62071
qloeo <https://github.com/qloeo>
2023-07-25 01:35:35 +0900

    [update]4주차

--- W04/boj_20920.py
+++ W04/boj_20920.py
@@ -1,16 +1,17 @@
 import sys
-from collections import defaultdict
+from collections import defaultdict # 존재하지 않는 키를 조회할 경우에 에러를 발생시키는 대신 기본값을 반환하는 딕셔너리의 서브클래스
 
 def solution():
-    N, M = map(int, sys.stdin.readline().split())
+    N, M = map(int, sys.stdin.readline().split()) # 총 단어갯수, M 길이 이상의 단어만 외운다.
     words = [sys.stdin.readline().strip() for _ in range(N)]
-    word_dict = defaultdict(int)
+    word_dict = defaultdict(int) # 딕셔너리 기본값 0
 
     for word in words:
         if len(word) >= M:
             word_dict[word] += 1
-
+    # 리스트의 각 단어에 대해, 단어의 길이가 M 이상일 경우 해당 단어의 빈도수를 증가
     sorted_words = sorted(word_dict.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+    # 빈도수가 높은 순, 단어의 길이가 긴 순, 알파벳 순서가 앞선 순서로 정렬
 
     for word in sorted_words:
         print(word[0])
--- W04/boj_2579.py
+++ W04/boj_2579.py
@@ -1,15 +1,19 @@
 # 계단오르기
-n = int(input())
-stairs = [0]*n
+n = int(input()) # 6
+stairs = [0] * n # 계단 리스트 생성
 for i in range(n):
     stairs[i] = int(input())
 
-dp = [0]*n
-dp[0] = stairs[0]
-if n > 1: dp[1] = stairs[0] + stairs[1]
-if n > 2: dp[2] = max(stairs[1] + stairs[2], stairs[0] + stairs[2])
+dp = [0] * n # 각 계단까지의 도달 점수
+dp[0] = stairs[0] # 첫번째 계단 도달 점수 = 첫번째계단
+if n > 1: dp[1] = stairs[0] + stairs[1] # 계단 두개 : 두번째 계단 도달 점수 = 첫번째계단 + 두번째 계단
+if n > 2: dp[2] = max(stairs[1] + stairs[2], stairs[0] + stairs[2]) # 계단 세개 : 세번째 계단 도달 점수 = max(두번째 계단 + 세번째계단 ,첫번째 계단 + 세번째 계단) 
 
-for i in range(3, n):
+for i in range(3, n): # 네번째 계단부터 마지막 계단까지 : n이 3 이하이면 수행하지않는다.
     dp[i] = max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i])
+               # 첫번째 10 + 세번째계단 15 + 네번째 25 , 두번째 20 + 네번째 25  중에 큰 점수
+print(dp[n-1]) # 인덱스는 0부터 이므로 n-1이 마지막 계단(인덱스)이다.
 
-print(dp[n-1])
+
+# 6
+# 10 20 15 25 10 20
