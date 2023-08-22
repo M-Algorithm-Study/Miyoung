7c47aab2ac7fee125e06f381aeee18c1802f36a0
qloeo <https://github.com/qloeo>
2023-08-22 09:43:50 +0900

    [UPDATE]8주차

--- W08/boj_1697.py
+++ W08/boj_1697.py
@@ -1,32 +1,32 @@
 from collections import deque
 
-def bfs(n, k):
-    MAX = 100001
-    visited = [0] * MAX
-    queue = deque([n])
+def bfs(n, k):     # n: 수빈(5), k: 동생(17)
+    MAX = 100001   # 범위의 최대값 + 1 (0부터 시작하기 때문)
+    v = [0] * MAX  # 각 위치에 도달하는데 걸린 시간을 저장하는 리스트
+    q = deque([n]) # 초기값 : 수빈이의 위치 q = [5]
 
-    while queue:
-        current = queue.popleft()
+    while q: 
+        c = q.popleft() # 최단 시간을 현재 시간으로 저장
         
-        if current == k:  # 동생을 찾았다면
-            return visited[current]
+        if c == k:  # 동생을 찾았다면
+            return v[c] # 그 위치에 도달하는데 걸린 시간을 출력
         
-        # 걷기 (current - 1)
-        if 0 <= current - 1 < MAX and not visited[current - 1]:
-            visited[current - 1] = visited[current] + 1
-            queue.append(current - 1)
+        # c - 1 이 범위 내에 있고, 방문하지 않은경우
+        if 0 <= c - 1 < MAX and not v[c - 1]:
+            v[c - 1] = v[c] + 1 # -1 경로에 걸린 시간 기록& 방문표시
+            q.append(c - 1) # 시간 -1
         
-        # 걷기 (current + 1)
-        if 0 <= current + 1 < MAX and not visited[current + 1]:
-            visited[current + 1] = visited[current] + 1
-            queue.append(current + 1)
+        # c + 1 이 범위 내에 있고, 방문하지 않은경우
+        if 0 <= c + 1 < MAX and not v[c + 1]:
+            v[c + 1] = v[c] + 1  # +1 경로에 걸린 시간 기록& 방문표시
+            q.append(c + 1)  # 시간 +1
         
-        # 순간이동 (current * 2)
-        if 0 <= current * 2 < MAX and not visited[current * 2]:
-            visited[current * 2] = visited[current] + 1
-            queue.append(current * 2)
+        # c * 2 가 범위 내에 있고, 방문하지 않은경우
+        if 0 <= c * 2 < MAX and not v[c * 2]: 
+            v[c * 2] = v[c] + 1 # *2 경로에 걸린 시간 기록& 방문표시
+            q.append(c * 2) # 시간 *2
             
-    return -1
+    return -1 # 동생을 찾지 못한경우 예외처리.
 
 n, k = map(int, input().split())
 print(bfs(n, k))
--- W08/boj_2638.py
+++ W08/boj_2638.py
@@ -1,45 +1,46 @@
 from collections import deque
 
-n, m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
+n, m = map(int, input().split()) 
+board = [list(map(int, input().split())) for _ in range(n)] # 치즈 위치
 dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dy = [0, 0, -1, 1] 
 
-def bfs():
-    q = deque([(0, 0)])
-    visited = [[False] * m for _ in range(n)]
-    visited[0][0] = True
-    board[0][0] = -1
+def bfs(): # 외부의 공기와 접촉하는 치즈 부분을 찾는 함수 
+    q = deque([(0, 0)]) # 시작점
+    v = [[False] * m for _ in range(n)] # 방문 2차원 리스트
+    v[0][0] = True # 시작점은 방문 체크
+    board[0][0] = -1 # 공기 접촉되는 부분이므로 -1
     
-    while q:
+    while q: 
         x, y = q.popleft()
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and board[nx][ny] <= 0:
-                board[nx][ny] = -1
-                visited[nx][ny] = True
+            nx, ny = x + dx[i], y + dy[i] # 상하좌우
+            # ny, nx가 범위 내에 있고, 방문하지 않았으며 그 위치 값이 0이거나 -1(공기접촉)
+            if 0 <= nx < n and 0 <= ny < m and not v[nx][ny] and board[nx][ny] <= 0:
+                board[nx][ny] = -1 # 공지 접촉 표시
+                v[nx][ny] = True   # 방문 체크
                 q.append((nx, ny))
 
-def melt():
+def melt(): # 외부 공기와 2면 이상 접촉하는 치즈를 찾아 녹이는 함수
     melting = []
-    for i in range(n):
-        for j in range(m):
-            if board[i][j] == 1:
+    for i in range(n):     # 모ㄴ 방향을 순회하며 치즈 찾기
+        for j in range(m): 
+            if board[i][j] == 1: # 현재 위치가 치즈라면
                 cnt = 0
-                for d in range(4):
+                for d in range(4): # 네 방향 중  몇 면이 외부공기와 접촉하는지 체크
                     ni, nj = i + dx[d], j + dy[d]
-                    if board[ni][nj] == -1:
+                    if board[ni][nj] == -1: # 공기와 접촉했다면 count
                         cnt += 1
                 if cnt >= 2:
                     melting.append((i, j))
     for x, y in melting:
         board[x][y] = -1
-    return len(melting) > 0
+    return len(melting) > 0 # 0보다 크면 True(melt 계속 진행), 0이면 False값을 받환해서 while문이 종료됨.
 
-time = 0
+time = 0 # 치즈가 모두 녹는 데 걸리는 시간
 while True:
-    bfs()
-    if not melt():
+    bfs() # 외부 공기와 접촉하는 치즈를 찾아서
+    if not melt(): # 치즈가 더 이상 녹지 않으면 반복을 종료
         break
     time += 1
 
