6523a7ebd0eda09770bacb47c767a07b9477209b
qloeo <https://github.com/qloeo>
2023-08-29 09:13:46 +0900

    [UPDATE]9주차

--- W09/13717.py
+++ W09/13717.py
@@ -1,25 +1,29 @@
-N = int(input())
-pokemons = []
-total_evolve_count = 0
+'''
+포켓몬 GO
+'''
+
+N = int(input()) # 포켓몬 마릿수
+pokemons = []    # 이름, 진화횟수 리스트
+total_evolve_count = 0 # 총 진화가능 횟수 
 
 for _ in range(N):
-    name = input().strip()
-    K, M = map(int, input().split())
-    evolve_count = 0
+    name = input().strip() # 이름
+    K, M = map(int, input().split()) # 진화시 필요한 사탕 수, 가지고있는 사탕 수 
+    evolve_count = 0 # 진화 횟수 count
 
-    while M >= K:
-        M -= K
-        M += 2
-        evolve_count += 1
+    while M >= K: # 가지고 있는사탕이 같거나 더 많으면 진행
+        M -= K # 진화시 필요한 사탕을 차감
+        M += 2 # 돌려받는 사탕 +2
+        evolve_count += 1 # 진화 횟수 +1
 
-    total_evolve_count += evolve_count
-    pokemons.append((name, evolve_count))
+    total_evolve_count += evolve_count # 총 진화 가능횟수 += 진화횟수 
+    pokemons.append((name, evolve_count)) # 이름과 횟수를 튜플로 저장
 
-# 모든 포켓몬 중에서 진화시킬 수 있는 최대 횟수를 찾습니다.
+# 포켓 몬 중 'x[1]' 즉 튜플의 두번째에 들어있는 횟수 중 가장 높은 값을 찾고, '[1]' 두번째 값만 추출하여 값을 반환 
 max_evolve = max(pokemons, key=lambda x: x[1])[1]
 
-# 최대 횟수로 진화시킬 수 있는 포켓몬 중에서 처음으로 나오는 포켓몬을 선택합니다.
+# if문 조건이 참일때, name을 반환
 max_evolve_pokemon = next(name for name, count in pokemons if count == max_evolve)
 
-print(total_evolve_count)
-print(max_evolve_pokemon)
+print(total_evolve_count) # 총 진화가능 횟수
+print(max_evolve_pokemon) # 가장많이 진화 할 수 있는 포키몬 이름
--- W09/16236.py
+++ W09/16236.py
@@ -4,50 +4,53 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 # BFS로 먹을 수 있는 물고기의 위치와 거리를 탐색
-def bfs(x, y, size):
-    v = [[0] * n for _ in range(n)]
-    v[x][y] = 1
-    queue = deque([(x, y)])
-    fishes = []
+def bfs(x, y, size): # 아기상어 좌표, 사이즈
+    v = [[0] * n for _ in range(n)] # 방문
+    v[x][y] = 1  # 시작점 방문처리
+    queue = deque([(x, y)]) # 시작점 추가
+    fishes = [] # [(거리, x, y)...] 아기상어와의 거리와 물고기 위치를 저장
     while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and not v[nx][ny]:
-                # 자신의 크기보다 큰 물고기의 칸은 지나갈 수 없다
+        x, y = queue.popleft() # 큐에서 위치를 pop
+        for i in range(4): # 상하좌우 확인
+            nx, ny = x + dx[i], y + dy[i] 
+            # 공간안에 있고, 방문하지 않았다면
+            if 0 <= nx < n and 0 <= ny < n and not v[nx][ny]: 
+                # 물고기가 아기상어보다 작거나 같고, 물고기가 없으면
                 if matrix[nx][ny] <= size or matrix[nx][ny] == 0:
-                    v[nx][ny] = v[x][y] + 1
-                    queue.append((nx, ny))
-                # 자신의 크기보다 작은 물고기만 먹을 수 있다
-                if 0 < matrix[nx][ny] < size:
-                    fishes.append((nx, ny, v[nx][ny] - 1))
+                    v[nx][ny] = v[x][y] + 1 # 거리 +1 (0이 아니면 방문)
+                    queue.append((nx, ny)) # 위지정보 업데이트 
+                # 자신의 크기보다 작은 물고기는 먹고 같으면 지나간다.
+                if 0 < matrix[nx][ny] < size: # 크기가 작으면 
+                    fishes.append((nx, ny, v[nx][ny] - 1)) # 
     return fishes
 
-n = int(input())
-matrix = []
-shark_size, shark_eat, shark_x, shark_y = 2, 0, 0, 0
 
-for i in range(n):
-    row = list(map(int, input().split()))
-    matrix.append(row)
-    for j in range(n):
-        if row[j] == 9:
-            shark_x, shark_y = i, j
-            matrix[i][j] = 0
 
-time = 0
-while True:
+n = int(input()) # 공간의 크기
+matrix = []  # 공간의 상태(물고기사이즈)
+shark_size, shark_eat, shark_x, shark_y = 2, 0, 0, 0 # 아기상어크기, 먹은 물고기수, 아기상어 위치
+
+for i in range(n): 
+    row = list(map(int, input().split())) # 행에대한 정보를 입력 받아서
+    matrix.append(row) # 2차원 리스트에 추가
+    for j in range(n): # 행의 칸을 확인
+        if row[j] == 9: # 아기상어 위치 확인
+            shark_x, shark_y = i, j # 위치 추가
+            matrix[i][j] = 0 # 아기상어 자리는 빈칸으로
+
+time = 0 # 소요시간
+while True: # 먹을 물고기가 없을때까지 반복
     fishes = bfs(shark_x, shark_y, shark_size)
     if not fishes:  # 먹을 수 있는 물고기가 없다면 종료
         break
     fishes.sort(key=lambda x: (x[2], x[0], x[1]))  # 거리, x좌표, y좌표 순으로 정렬
-    fish_x, fish_y, fish_distance = fishes[0]
-    time += fish_distance
-    matrix[fish_x][fish_y] = 0
-    shark_eat += 1
-    if shark_eat == shark_size:
-        shark_size += 1
-        shark_eat = 0
-    shark_x, shark_y = fish_x, fish_y
+    fish_x, fish_y, fish_distance = fishes[0] # 가장 가까운 물고기 정보를 넣는다.
+    time += fish_distance # 시간 업데이트
+    matrix[fish_x][fish_y] = 0 # 물고기를 먹고 빈칸으로
+    shark_eat += 1 # 먹은 물고기 수 증가
+    if shark_eat == shark_size: 
+        shark_size += 1 # 아기상어 크기증가
+        shark_eat = 0  # 먹은 물고기 수 초기화
+    shark_x, shark_y = fish_x, fish_y # 상어 위치이동
 
 print(time)
