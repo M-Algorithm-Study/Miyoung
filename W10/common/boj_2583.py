762890506bd9bdfca0d60513652c1c6a05eaf563
qloeo <https://github.com/qloeo>
2023-09-04 22:05:51 +0900

    [UPDATE]10주차

--- W10/common/boj_2583.py
+++ W10/common/boj_2583.py
@@ -3,11 +3,11 @@ from collections import deque
 M, N, K = map(int, input().split())
 paper = [[0 for _ in range(N)] for _ in range(M)]
 
-for _ in range(K):
+for _ in range(K): # 직사각형
     x1, y1, x2, y2 = map(int, input().split())
     for i in range(M-y2, M-y1):
         for j in range(x1, x2):
-            paper[i][j] = 1
+            paper[i][j] = 1 # 직사각형 구역을 1로 표시
 
 # 상, 하, 좌, 우
 dx = [-1, 1, 0, 0]
@@ -16,8 +16,8 @@ dy = [0, 0, -1, 1]
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
-    paper[x][y] = 1
-    count = 1
+    paper[x][y] = 1  # 시작점 방문표시
+    count = 1  # 영역크기 증가
 
     while queue:
         x, y = queue.popleft()
@@ -26,10 +26,11 @@ def bfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
 
+            # 영역 내이고, 방문 안한경우
             if 0 <= nx < M and 0 <= ny < N and paper[nx][ny] == 0:
-                queue.append((nx, ny))
-                paper[nx][ny] = 1
-                count += 1
+                queue.append((nx, ny)) # 큐에 추가
+                paper[nx][ny] = 1 # 방문 표시
+                count += 1 # 영역크기 증가
 
     return count
 
@@ -38,7 +39,8 @@ areas = []
 for i in range(M):
     for j in range(N):
         if paper[i][j] == 0:
-            areas.append(bfs(i, j))
+            areas.append(bfs(i, j)) 
+            # return count가 되면서 areas리스트에 count가 하나씩 추가된다.
 
 print(len(areas))
 areas.sort()
