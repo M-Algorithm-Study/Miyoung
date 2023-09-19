3ceff74d85aaa969ba519b1e47239cd9d7d28b3e
qloeo <https://github.com/qloeo>
2023-09-19 14:20:59 +0900

    [UPDATE]11주차

--- W11/common/boj_14719.py
+++ W11/common/boj_14719.py
@@ -1,3 +1,6 @@
+# H, W = 4, 8
+# blocks = [ 3, 1, 2, 3, 4, 1, 1, 2 ]
+
 H, W = map(int, input().split())
 blocks = list(map(int, input().split()))
 
@@ -19,3 +22,15 @@ for i in range(W):
     total_water += min(left_max[i], right_max[i]) - blocks[i]
 
 print(total_water)
+
+
+'''
+  min
+  ⌒ ⌒ ⌒ ⌒ ⌒ ⌒ ⌒ ⌒
+  3 3 3 3 4 4 4 4
+  4 4 4 4 4 2 2 2
+  ⏝ ⏝ ⏝ ⏝ ⏝ ⏝ ⏝
+- 3 1 2 3 4 1 1 2
+-----------------
+  0 2 1 0 0 1 1 0
+'''
--- W11/common/boj_9251.py
+++ W11/common/boj_9251.py
@@ -1,13 +1,28 @@
-str1 = input()
-str2 = input()
+# str1 = 'ACAYKP'
+# str2 = 'CAPCAK'
+
+str1 = input().rstrip()
+str2 = input().rstrip()
 
 dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
 
 for i in range(1, len(str1) + 1):
     for j in range(1, len(str2) + 1):
         if str1[i-1] == str2[j-1]:
-            dp[i][j] = dp[i-1][j-1] + 1
+            dp[i][j] = dp[i-1][j-1] + 1 # 같으면 -1,-1  에 + 1
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]) # 다르면 왼쪽과 위쪽 중 max값
 
 print(dp[-1][-1])
+
+'''
+     0 C A P C A K
+  0  0 0 0 0 0 0 0
+  A  0 0 1 1 1 1 1
+  C  0 1 1 1 2 2 2
+  A  0 1 2 2 2 3 3
+  Y  0 1 2 2 2 3 3
+  K  0 1 2 2 2 3 4
+  P  0 1 2 3 3 3 4
+
+'''
